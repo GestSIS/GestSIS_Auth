653546431ac43827c2c24e66e6768657d18f7707
bastien wermeille <bastien.wermeille@gmail.com>
2022-04-04 18:02:11 +0200

    Migrate jwtTokenValidator

--- app/Http/Middleware/JwtTokenValidatorRole.php
+++ app/Http/Middleware/JwtTokenValidatorRole.php
@@ -16,7 +16,7 @@ class JwtTokenValidatorRole
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle(Request $request, Closure $next, $role = Null)
+    public function handle(Request $request, Closure $next, ...$roles)
     {
         if (env('APP_ENV') === 'testing') {
             return $next($request);
@@ -28,20 +28,21 @@ class JwtTokenValidatorRole
             return response()->json(["error" => "Accès refusé"], 401);
         }
 
+
         $sisKey = $request->header('Sis-Id', Null);
-        if (is_null($sisKey) && !(is_null($role) || $role == '')) {
+        if (is_null($sisKey)) {
             return response()->json(["error" => "Sis non sélectionné"], 401);
         }
 
-        if (!is_null($role) && $role != '') {
+        if (count($roles) > 0) {
             // Check has role for provided sis
             $perms = (array) $token->data->permissions;
             if (!array_key_exists($sisKey, $perms)) {
                 return response()->json(["error" => "Aucun droit pour ce sis"], 401);
             }
 
-            if (!in_array($role, $perms[$sisKey])) {
-                return response()->json(["error" => "Rôle " . join(", ", $role) . " manquant"], 401);
+            if (count(array_intersect($roles, $perms[$sisKey])) == 0) {
+                return response()->json(["error" => "Au moins 1 des rôles suivant est requis [" . join(", ", $roles) . "]."], 401);
             }
         }
         return $next($request);
