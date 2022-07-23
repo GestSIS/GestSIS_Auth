6872e52b65430338d8d52a1ed05b294aad4283d1
Bastien Wermeille <bastien.wermeille@gmail.com>
2022-07-23 21:51:23 +0200

    Change jwt format of admin user

--- app/Http/Middleware/JwtTokenValidatorRole.php
+++ app/Http/Middleware/JwtTokenValidatorRole.php
@@ -18,10 +18,6 @@ class JwtTokenValidatorRole
      */
     public function handle(Request $request, Closure $next, ...$roles)
     {
-        if (env('APP_ENV') === 'testing') {
-            return $next($request);
-        }
-
         try {
             $token = TokenTools::validateToken($request->bearerToken());
         } catch (Exception $e) {
@@ -34,14 +30,16 @@ class JwtTokenValidatorRole
                 return response()->json(["error" => "Sis non sélectionné"], 401);
             }
 
-            // Check has role for provided sis
-            $perms = (array) $token->data->permissions;
-            if (!array_key_exists($sisKey, $perms)) {
-                return response()->json(["error" => "Aucun droit pour ce sis"], 401);
-            }
+            if ($token->data->admin !== true) {
+                // Check has role for provided sis
+                $perms = (array) $token->data->permissions;
+                if (!array_key_exists($sisKey, $perms)) {
+                    return response()->json(["error" => "Aucun droit pour ce sis"], 401);
+                }
 
-            if (count(array_intersect($roles, $perms[$sisKey])) == 0) {
-                return response()->json(["error" => "Au moins 1 des rôles suivant est requis [" . join(", ", $roles) . "]."], 401);
+                if (count(array_intersect($roles, $perms[$sisKey])) == 0) {
+                    return response()->json(["error" => "Au moins 1 des rôles suivant est requis [" . join(", ", $roles) . "]."], 401);
+                }
             }
         }
         return $next($request);
