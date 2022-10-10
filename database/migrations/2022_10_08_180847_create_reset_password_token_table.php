02a1deab45ab4a8f8ed949e7705098baa7faea38
Bastien Wermeille <bastien.wermeille@gmail.com>
2022-10-10 08:47:30 +0200

    Add id for reset password tokens

--- database/migrations/2022_10_08_180847_create_reset_password_token_table.php
+++ database/migrations/2022_10_08_180847_create_reset_password_token_table.php
@@ -14,6 +14,7 @@ class CreateResetPasswordTokenTable extends Migration
     public function up()
     {
         Schema::create('reset_password_tokens', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
 
             $table->string('token');
