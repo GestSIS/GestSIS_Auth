ef24b6b7b61eed80d2a2675a9c881f33b514a13c
Bastien Wermeille <bastien.wermeille@gmail.com>
2022-11-11 23:08:09 +0100

    Casts pour sapeurs

--- app/Sapeur.php
+++ app/Sapeur.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Sapeur extends Model
 {
     use HasFactory;
+
+    protected $casts = ['sapeur_id' => 'integer', 'sis_id' => 'integer', 'user_id' => 'integer'];
 }
