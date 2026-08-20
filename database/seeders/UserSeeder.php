faabb57f726012b7dca050e92c88e64c55a303a7
Bastien Wermeille <bastien.wermeille@gmail.com>
2026-08-20 10:59:51 +0200

    fix user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,7 +18,7 @@ class UserSeeder extends Seeder
     {
         DB::table('users')->insert([
             ['id' => 1, 'name' => 'admin', 'admin' => true, 'email' => 'admin@gestsis.ch', 'email_verified_at' => Carbon::yesterday(), 'password' => Hash::make('apptest')],
-            ['id' => 1, 'name' => 'admin', 'admin' => true, 'email' => 'user@gestsis.ch', 'email_verified_at' => Carbon::yesterday(), 'password' => Hash::make('apptest')],
+            ['id' => 2, 'name' => 'demo', 'admin' => false, 'email' => 'demo@gestsis.ch', 'email_verified_at' => Carbon::yesterday(), 'password' => Hash::make('apptest')],
         ]);
 
         DB::table('user_roles')->insert([
@@ -28,7 +28,7 @@ class UserSeeder extends Seeder
 
         DB::table('sapeurs')->insert([
             ['user_id' => 1, 'sapeur_id' => 1, 'sis_id' => 1],
-            ['user_id' => 1, 'sapeur_id' => 1, 'sis_id' => 1],
+            ['user_id' => 2, 'sapeur_id' => 2, 'sis_id' => 1],
         ]);
     }
 }
